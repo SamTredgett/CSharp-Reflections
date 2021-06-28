2321ba37e55468b0b53ca7426ef993e2add11d63
Sam Tredgett <sam.tredgett@gmail.com>
2021-06-28 12:00:22 +0100

    made changes to the new Car.cs class and added in getters and a ToString override

---
+++ Car.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace Reflection
+{
+    class Car 
+    {
+        private string make;
+        private string model;
+        private string registration;
+        private double year;
+        private double currentValue;
+
+        public Car(string Make, string Model, string Registration, double Year, double CurrentVal)
+        {
+          this.make = Make;
+          this.model = Model;
+          this.registration = Registration;
+          this.year = Year;
+          this.currentValue = CurrentVal; 
+        }
+
+        public double getValue() => return this.currentValue;
+        public double getYear() => return this.year;
+        public override string ToString() { 
+            return $"This car is a {this.make} {this.model} with registration {this.registration} made in {this.year} with a value of {this.currentValue}"
+        }
+
+    }
+}
